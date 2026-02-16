aa0fbc36757f27e2ea06bfcc68ac79bf9ff573f8
lesptisakhi <les.ptis.akhi@gmail.com>
2026-02-16 23:12:34 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ MEGA_URL="https://mega.nz/file/l6UFTQKQ#PUeKYD2dFgbHONUMsXbkIo6OgWa-TOvuQxRFCg7j
 OUTPUT="video.mp4"
 
 # Téléchargement depuis MEGA
-megadl "$MEGA_URL" --path "$OUTPUT"
+megatools dl "$MEGA_URL" --path "$OUTPUT"
 
 # URL Instagram RTMPS complète
 INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/18110039119739471?s_bl=1&s_fbp=cdg4-3&s_ow=10&s_prp=mrs2-3&s_sw=0&s_tids=1&s_vt=ig&a=Ab43XeMsXk9BN-K9g4hEPEFO"
