63ef34af3587740325d2675ab90fe38dd9ea86dd
lesptisakhi <les.ptis.akhi@gmail.com>
2026-02-16 23:09:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-VIDEO_URL="https://drive.google.com/uc?export=download&id=1puZmFkf6lC9vnfAoEmx9Nc560imjCaxZ"
+# Lien MEGA
+MEGA_URL="https://mega.nz/file/l6UFTQKQ#PUeKYD2dFgbHONUMsXbkIo6OgWa-TOvuQxRFCg7jxg0"
 
-INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/TA_CLE_INSTAGRAM"
+# Nom du fichier local
+OUTPUT="video.mp4"
 
-ffmpeg -re -i "$VIDEO_URL" \
+# Téléchargement depuis MEGA
+megadl "$MEGA_URL" --path "$OUTPUT"
+
+# Lien RTMPS Instagram
+INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/18110039119739471?s_bl=1&s_fbp=cdg4-3&s_ow=10&s_prp=mrs2-3&s_sw=0&s_tids=1&s_vt=ig&a=Ab43XeMsXk9BN-K9g4hEPEFO"
+
+# Lancement du live
+ffmpeg -re -i "$OUTPUT" \
 -vcodec libx264 -preset veryfast -pix_fmt yuv420p \
 -acodec aac -f flv "$INSTAGRAM_URL"
