bb89c8b5df67f2a68f53ef05261c0f566d84a038
lesptisakhi <les.ptis.akhi@gmail.com>
2026-02-16 22:53:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VIDEO_URL="https://drive.google.com/file/d/1puZmFkf6lC9vnfAoEmx9Nc560imjCaxZ/view?usp=drive_link"
+VIDEO_URL="https://drive.google.com/uc?export=download&id=1puZmFkf6lC9vnfAoEmx9Nc560imjCaxZ"
 
 INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/TA_CLE_INSTAGRAM"
 
