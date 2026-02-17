eb2ff937a2a61f9c373c7c413699a00d2ba194d0
lesptisakhi <les.ptis.akhi@gmail.com>
2026-02-17 11:07:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ while [ ! -f "$OUTPUT" ]; do
     sleep 1
 done
 
-INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/17891451369422874?s_bl=1&s_fbp=cdg4-3&s_ow=10&s_prp=mrs2-3&s_sw=0&s_tids=1&s_vt=ig&a=Ab7W1RziYdToDPQ3BdH_MchA"
+INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/18093696784817265?s_bl=1&s_fbp=cdg4-3&s_ow=10&s_prp=mrs2-3&s_sw=0&s_tids=1&s_vt=ig&a=Ab7LK77ntcJhCmzSCD-zs7Ya"
 
 # Live Instagram (720p, 3.5 Mb/s)
 ffmpeg -re -i "$OUTPUT" \
