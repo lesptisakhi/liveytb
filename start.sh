33cf377a53233ad292536662240286caa07a0c46
lesptisakhi <les.ptis.akhi@gmail.com>
2026-02-17 10:31:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Lien MEGA
 MEGA_URL="https://mega.nz/file/l6UFTQKQ#PUeKYD2dFgbHONUMsXbkIo6OgWa-TOvuQxRFCg7jxg0"
-
-# Nom du fichier local
 OUTPUT="video.mp4"
 
-# Téléchargement depuis MEGA
+# Téléchargement MEGA
 megatools dl "$MEGA_URL" --path "$OUTPUT"
 
-# URL Instagram RTMPS complète
-INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/18110039119739471?s_bl=1&s_fbp=cdg4-3&s_ow=10&s_prp=mrs2-3&s_sw=0&s_tids=1&s_vt=ig&a=Ab43XeMsXk9BN-K9g4hEPEFO"
+# URL Instagram
+INSTAGRAM_URL="rtmps://edgetee-upload-mrs2-3.xx.fbcdn.net:443/rtmp/17891451369422874?s_bl=1&s_fbp=cdg4-3&s_ow=10&s_prp=mrs2-3&s_sw=0&s_tids=1&s_vt=ig&a=Ab7W1RziYdToDPQ3BdH_MchA"
 
-# Lancement du live
+# Live Instagram (720p, 3.5 Mb/s)
 ffmpeg -re -i "$OUTPUT" \
 -vcodec libx264 -preset veryfast -pix_fmt yuv420p \
--acodec aac -f flv "$INSTAGRAM_URL"
+-b:v 3500k -maxrate 3500k -bufsize 7000k \
+-vf "scale=1280:720" \
+-acodec aac -b:a 128k -ar 44100 \
+-f flv "$INSTAGRAM_URL"
